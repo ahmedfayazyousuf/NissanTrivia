3b7e44a6ef9a517172e89dfcbabcfd5f3f82da1a
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2022-12-29 12:23:29 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import "./App.css";
+import 'bootstrap';
+
+import Home from "./components/Home/Home";
+import QR from './components/QR';
+import EnterCode from './components/EnterCode';
+import Success from "./components/Success/index";
+import Login from './components/Login';
+import Instructions from './components/Instructions';
+import Q1 from './components/Questions/Q1';
+import Q2 from './components/Questions/Q2';
+import Q3 from './components/Questions/Q3';
+import Q4 from './components/Questions/Q4';
+import Q5 from './components/Questions/Q5';
+import Error from './components/Error/Error';
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/QR" element={<QR />} />
+          <Route path="/EnterCode" element={<EnterCode />} />
+          <Route path="/Success/:user_id" element={<Success />} />
+          <Route path="/Login/:user_id" element={<Login />} />
+
+          <Route path="/Instructions" element={<Instructions />} />
+          <Route path="/Q1" element={<Q1 />} />
+          <Route path="/Q2" element={<Q2 />} />
+          <Route path="/Q3" element={<Q3 />} />
+          <Route path="/Q4" element={<Q4 />} />
+          <Route path="/Q5" element={<Q5 />} />
+
+          <Route path="*" element={<Error />} />
+      </Routes>
+    </>
+  )
+}
+export default App
---
+++ src/components/Questions/Q4/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Q4 = () => { 
+    return (
+        <>
+            <div className="main">
+                <div className="form-items">
+                    {/* <div style={{justifyContent: 'center', alignItems: 'center'}}>
+                        <img id='head' style={{width: '150px'}} src={nislogo} alt="Nissan Laptop Cover"/>
+                    </div> */}
+                    <h1 className='thankk' style={{fontWeight:'1000', marginTop: '15px'}}>THANK YOU!</h1>
+                    <h4 style={{fontSize:'16px'}}>Your response has been recorded.</h4>
+                    <h4 style={{fontSize:'16px'}}>You will receive a confirmation email shortly.</h4>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Q4
