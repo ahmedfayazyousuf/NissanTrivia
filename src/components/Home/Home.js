8ba2005fd34991924ad7bc9933738d42ab36a53c
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2023-01-04 01:05:30 +0500

    frontend for registration page
    frontend for registration page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import nislogo from './nislogo.png'
+import NissanLogo from '../Z_Images/nissanlogo.png'
+import HowWell from '../Z_Images/howwell.png'
+import GetReady from '../Z_Images/getreadytoexplorewhite.png'
+import '../Z_Styles/Nissan.css'
 const Home = () => { 
 
     return (
-        <>
-            <div className="main">
-                <div className="form-items">
-                    <div style={{justifyContent: 'center', alignItems: 'center'}}>
-                        <img id='head' style={{width: '150px'}} src={nislogo} alt="Nissan Laptop Cover"/>
-                    </div>
-                    
-                    <h1 style={{fontWeight:'1000', fontSize: '60px', marginTop: '15px'}}>WELCOME</h1>
-                    <h4> To The Nissan RSVP Application </h4>
+        <div>
+            
+            <div style={{display: 'flex', flexDirection: 'column', width: '100vw', height: '100vh', justifyContent: 'center', alignItems:'center', textAlign: 'center'}}>
+                <div style={{justifyContent: 'center', alignItems: 'center'}}>
+                    <img id='head' style={{width: '130px', marginTop: '40px'}} src={NissanLogo} alt="Nissan Logo"/>
                 </div>
+
+                <div>
+                    <img id='head' style={{width: '250px', marginTop: '18px', marginBottom: '80px'}} src={HowWell} alt="How Well"/>
+                </div>
+
+                <div style={{display: 'flex', flexDirection: 'column', width: '350px', gap:'10px', alignItems: 'center'}}>
+                        <input type="email" id='email' placeholder='ENTER YOUR EMAIL' style={{width:"220px", height: '25px', textAlign: 'center', fontSize: '8px'}} />
+                        <input type="loccode" id="loccode" placeholder='ENTER LOCATION CODE' style={{width:"220px", height: '25px', textAlign: 'center', fontSize: '8px'}} /> 
+                    <button className="grab" style={{width:"200px", height: '35px', marginTop: '20px', margin: '10px', borderRadius: '10px', backgroundColor: 'white'}}>LET'S PLAY</button>
+                </div>
+
+                <div className="form-check" id="checkthreewrap" style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                    <input className="form-check-input" id="checkthree" type="checkbox" value="" style={{backgroundColor: '#061A1B', borderColor: 'white', Size: '8px'}} required/>
+                    <label className="form-check-label" style={{fontSize: '8px', color:'white'}}>Agree to terms and conditions.</label>
+                </div>
+
+                <div>
+                    <img id='head' style={{width: '150px', marginTop: '25px', marginBottom: '80px'}} src={GetReady} alt="How Well"/>
+                </div>
+
             </div>
-        </>
+        </div>
     )
 }
 
--- src/components/QR/index.js
+++ src/components/QR/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import {QrReader} from "react-qr-reader";
-import './qr.css'
 const QR = () => { 
     return (
         <div className="qrscan">
@@ -8,17 +6,7 @@ const QR = () => {
 
             </div>
             <div className="scanframe">
-            <QrReader
-                constraints = {{facingMode: 'environment'} }
-                scanDelay={5000}
-                //style={previewStyle}
-                // onError={handleError}
-                // onResult={handleScan}
-                videoStyle={{height:'100%',width:'97%',padding:0,left:4,top:0,alignSelf:'center'}}
-                videoContainerStyle={{height:'100%', width:'100%',left:0,padding:0}}
-                containerStyle={{height:'80%',left:'15%',width:'70%',top:0}}
-
-            />
+            
 
             <img id='scantext' alt='scan the next qr code' src='/Assets/scantext.png' style={{zIndex:"10000", position:"absolute",color:"white", width:"55%",
             marginBottom:"20%", visibility: "hidden"
