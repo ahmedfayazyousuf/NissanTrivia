2e61fae87e6dccc729c98ff9022a6b48fae5b444
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2023-01-04 18:59:59 +0500

    m
    m

--- src/components/Questions/Infiniti/QX55/Q1/index.js
+++ src/components/Questions/Infiniti/QX55/Q1/index.js
@@ -1,87 +1,96 @@
-// import NissanLogo from '../Z_Images/nissanlogo.png'
-// import GetReady from '../Z_Images/getreadytoexplorewhite.png'
-import '../../../../Z_Styles/Nissan.css'
-
+import React from "react";
+import { useState } from "react"
 
 const QX55Q1 = () => {
-
-        function Handleclick(e){
-            if(e === 'option1'){
-                document.getElementById(e).style.background = "black";
-                document.getElementById(e).style.color = "white";
-                document.getElementById('option2').style.background = "transparent";
-                document.getElementById('option2').style.color = "black";
-                document.getElementById('option3').style.background = "transparent";
-                document.getElementById('option3').style.color = "black";
-                document.getElementById('option4').style.background = "transparent";
-                document.getElementById('option4').style.color = "black";
-            }
-
-            if(e === 'option2'){
-                document.getElementById(e).style.background = "black";
-                document.getElementById(e).style.color = "white";
-                document.getElementById('option1').style.background = "transparent";
-                document.getElementById('option1').style.color = "black";
-                document.getElementById('option3').style.color = "black";
-                document.getElementById('option3').style.background = "transparent";
-                document.getElementById('option4').style.background = "transparent";
-                document.getElementById('option4').style.color = "black";
-            }
-
-            if(e === 'option3'){
-                document.getElementById(e).style.background = "black";
-                document.getElementById(e).style.color = "white";
-                document.getElementById('option1').style.background = "transparent";
-                document.getElementById('option1').style.color = "black";
-                document.getElementById('option2').style.background = "transparent";
-                document.getElementById('option2').style.color = "black";
-                document.getElementById('option4').style.background = "transparent";
-                document.getElementById('option4').style.color = "black";
-            }
-
-            if(e === 'option4'){
-                document.getElementById(e).style.background = "black";
-                document.getElementById(e).style.color = "white";
-                document.getElementById('option1').style.background = "transparent";
-                document.getElementById('option1').style.color = "black";
-                document.getElementById('option2').style.background = "transparent";
-                document.getElementById('option2').style.color = "black";
-                document.getElementById('option3').style.background = "transparent";
-                document.getElementById('option3').style.color = "black";
-            }
+    const [bool, setBool] = useState(false); 
+    function Handleclick(e){
+        if(e === 'option1'){
+            document.getElementById(e).style.background = "black";
+            document.getElementById(e).style.color = "white";
+            document.getElementById('option2').style.background = "transparent";
+            document.getElementById('option2').style.color = "black";
+            document.getElementById('option3').style.background = "transparent";
+            document.getElementById('option3').style.color = "black";
+            document.getElementById('option4').style.background = "transparent";
+            document.getElementById('option4').style.color = "black";
+            setBool(false);
         }
-    
-    return(
-        <div style={{backgroundColor: 'white', height: '100vh', width: '100vw', overflowY: 'hidden'}}>
+
+        if(e === 'option2'){
+            document.getElementById(e).style.background = "black";
+            document.getElementById(e).style.color = "white";
+            document.getElementById('option1').style.background = "transparent";
+            document.getElementById('option1').style.color = "black";
+            document.getElementById('option3').style.color = "black";
+            document.getElementById('option3').style.background = "transparent";
+            document.getElementById('option4').style.background = "transparent";
+            document.getElementById('option4').style.color = "black";
+            setBool(false);
+        }
+
+        if(e === 'option3'){
+            document.getElementById(e).style.background = "black";
+            document.getElementById(e).style.color = "white";
+            document.getElementById('option1').style.background = "transparent";
+            document.getElementById('option1').style.color = "black";
+            document.getElementById('option2').style.background = "transparent";
+            document.getElementById('option2').style.color = "black";
+            document.getElementById('option4').style.background = "transparent";
+            document.getElementById('option4').style.color = "black";
+            setBool(false);
+        }
+
+        if(e === 'option4'){
+            document.getElementById(e).style.background = "black";
+            document.getElementById(e).style.color = "white";
+            document.getElementById('option1').style.background = "transparent";
+            document.getElementById('option1').style.color = "black";
+            document.getElementById('option2').style.background = "transparent";
+            document.getElementById('option2').style.color = "black";
+            document.getElementById('option3').style.background = "transparent";
+            document.getElementById('option3').style.color = "black";
+            setBool(true);
+        }
+    }
+    function handleSubmit(){
+        if(bool === false){
+
+        }
+
+        else{
+
+        }
+    }
+
+return(
+    <div style={{backgroundColor: 'white', height: '100vh', width: '100vw', overflowY: 'hidden'}}>
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+            <img src="https://firebasestorage.googleapis.com/v0/b/assetuploadformbuilder.appspot.com/o/images%2Ftrivia.png?alt=media&token=a5975b8d-fd68-46ae-a90d-9acc6322df7f" alt="Logo" style={{width: '250px', marginTop: '60px', marginBottom: '20px'}}/>
+        </div>
+
+        <div style={{display: 'flex', flexDirection: 'column', textAlign: 'center', margin: '0', padding: '0', paddingLeft: '30px', paddingRight: '30px'}} >
+            <p style={{fontSize: '15px', marginTop: '10px'}} >What inspired the design of the tail lights on the QX55?</p>
+        </div>
+
+        <div className="slotparent"  style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center', width: '100%', marginTop: '15px'}}>
+            <div className="slotdiv" style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems:'center', width: '80%'}} id='slotparent'>
+                <button className="grab" id="option1" onClick={() => {Handleclick("option1")}} value="option1" style={{width:"170px", margin: '10px', border:'1px solid black', borderRadius: '20px', padding: '10px', color: 'black', backgroundColor: 'transparent', cursor: 'grab'}}>Kimono Fold</button>
+                <button className="grab" id="option2" onClick={() => {Handleclick("option2")}} value="option2" style={{width:"170px", margin: '10px', border:'1px solid black', borderRadius: '20px', padding: '10px', color: 'black', backgroundColor: 'transparent', cursor: 'grab'}}>Human Eye</button>
+            </div>
+
+            <div className="slotdiv" style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems:'center', width: '80%'}}>
+                <button className="grab" id="option3" onClick={() => {Handleclick("option3")}} value="option3" style={{width:"170px", margin: '10px', border:'1px solid black', borderRadius: '20px', padding: '10px', color: 'black', backgroundColor: 'transparent', cursor: 'grab'}}>Zebra</button>
+                <button className="grab" id="option4" onClick={() => {Handleclick("option4")}} value="option4" style={{width:"170px", margin: '10px', border:'1px solid black', borderRadius: '20px', padding: '10px', color: 'black', backgroundColor: 'transparent', cursor: 'grab'}}>Piano Keys</button>
+            </div>
+
+            <button className="grab" style={{width:"220px", marginTop: '40px', marginBottom: '40px', borderRadius: '10px', padding: '10px', color: 'white', backgroundColor: 'black', cursor: 'grab'}}>NEXT</button>
+
             <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-                <img src="https://firebasestorage.googleapis.com/v0/b/assetuploadformbuilder.appspot.com/o/images%2Ftrivia.png?alt=media&token=a5975b8d-fd68-46ae-a90d-9acc6322df7f" alt="Logo" style={{width: '250px', marginTop: '60px', marginBottom: '20px'}}/>
-            </div>
-
-            <div style={{display: 'flex', flexDirection: 'column', textAlign: 'center', margin: '0', padding: '0'}} >
-                <p style={{fontSize: '15px', marginTop: '10px'}} >Question Text?</p>
-            </div>
-
-            <div className="slotparent"  style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center', width: '100%', marginTop: '15px'}}>
-                <div className="slotdiv" style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems:'center', width: '70%'}} id='slotparent'>
-                    <button className="grab" id="option1" onClick={() => {Handleclick("option1")}} value="option1" style={{width:"150px", margin: '10px', border:'1px solid black', borderRadius: '20px', padding: '10px', color: 'black', backgroundColor: 'transparent', cursor: 'grab'}}>Option 1</button>
-                    <button className="grab" id="option2" onClick={() => {Handleclick("option2")}} value="option2" style={{width:"150px", margin: '10px', border:'1px solid black', borderRadius: '20px', padding: '10px', color: 'black', backgroundColor: 'transparent', cursor: 'grab'}}>Option 2</button>
-                </div>
-
-                <div className="slotdiv" style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems:'center', width: '70%'}}>
-                    <button className="grab" id="option3" onClick={() => {Handleclick("option3")}} value="option3" style={{width:"150px", margin: '10px', border:'1px solid black', borderRadius: '20px', padding: '10px', color: 'black', backgroundColor: 'transparent', cursor: 'grab'}}>Option 3</button>
-                    <button className="grab" id="option4" onClick={() => {Handleclick("option4")}} value="option4" style={{width:"150px", margin: '10px', border:'1px solid black', borderRadius: '20px', padding: '10px', color: 'black', backgroundColor: 'transparent', cursor: 'grab'}}>Option 4</button>
-                </div>
-
-                <button className="grab" style={{width:"220px", marginTop: '40px', marginBottom: '40px', borderRadius: '10px', padding: '10px', color: 'white', backgroundColor: 'black', cursor: 'grab'}}>NEXT</button>
-
-                <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-                    <img src="https://firebasestorage.googleapis.com/v0/b/assetuploadformbuilder.appspot.com/o/images%2Fgetreadyexplore.png?alt=media&token=bedd3483-ca06-414d-940e-9185a0b808a1" alt="Logo" style={{width: '20vh', marginTop: '30px', marginBottom: '10px'}}/>
-                </div>
-
-
+                <img src="https://firebasestorage.googleapis.com/v0/b/assetuploadformbuilder.appspot.com/o/images%2Fgetreadyexplore.png?alt=media&token=bedd3483-ca06-414d-940e-9185a0b808a1" alt="Logo" style={{width: '20vh', marginTop: '30px', marginBottom: '10px'}}/>
             </div>
         </div>
-    )
+    </div>
+)
 }
 
 export default QX55Q1
