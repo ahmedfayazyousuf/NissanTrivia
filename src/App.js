9d8a196a5a3d2f0aa6943ab3413f98a694c76aea
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2023-01-04 09:20:27 +0500

    h
    g

--- src/App.js
+++ src/App.js
@@ -9,11 +9,7 @@ import EnterCode from './components/EnterCode';
 import Success from "./components/Success/index";
 import Login from './components/Login';
 import Instructions from './components/Instructions';
-import Q1 from './components/Questions/Q1';
-import Q2 from './components/Questions/Q2';
-import Q3 from './components/Questions/Q3';
-import Q4 from './components/Questions/Q4';
-import Q5 from './components/Questions/Q5';
+import AltimaQ1 from './components/Questions/Nissan/Altima/Q1'
 import Error from './components/Error/Error';
 
 const App = () => {
@@ -25,13 +21,8 @@ const App = () => {
           <Route path="/EnterCode" element={<EnterCode />} />
           <Route path="/Success/:user_id" element={<Success />} />
           <Route path="/Login" element={<Login />} />
-
           <Route path="/Instructions" element={<Instructions />} />
-          <Route path="/Q1" element={<Q1 />} />
-          <Route path="/Q2" element={<Q2 />} />
-          <Route path="/Q3" element={<Q3 />} />
-          <Route path="/Q4" element={<Q4 />} />
-          <Route path="/Q5" element={<Q5 />} />
+          <Route path="/AltimaQ1" element={<AltimaQ1 />} />
 
           <Route path="*" element={<Error />} />
       </Routes>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,26 +10,26 @@ const Home = () => {
             
             <div style={{display: 'flex', flexDirection: 'column', width: '100vw', height: '100vh', justifyContent: 'center', alignItems:'center', textAlign: 'center'}}>
                 <div style={{justifyContent: 'center', alignItems: 'center'}}>
-                    <img id='head' style={{width: '130px', marginTop: '40px'}} src={NissanLogo} alt="Nissan Logo"/>
+                    <img id='head' style={{width: '130px', marginTop: '0px'}} src={NissanLogo} alt="Nissan Logo"/>
                 </div>
 
                 <div>
-                    <img id='head' style={{width: '250px', marginTop: '18px', marginBottom: '80px'}} src={HowWell} alt="How Well"/>
+                    <img id='head' style={{width: '300px', marginTop: '18px', marginBottom: '100px'}} src={HowWell} alt="How Well"/>
                 </div>
 
                 <div style={{display: 'flex', flexDirection: 'column', width: '350px', gap:'10px', alignItems: 'center'}}>
-                        <input type="email" id='email' placeholder='ENTER YOUR EMAIL' style={{width:"220px", height: '25px', textAlign: 'center', fontSize: '8px'}} />
-                        <input type="loccode" id="loccode" placeholder='ENTER LOCATION CODE' style={{width:"220px", height: '25px', textAlign: 'center', fontSize: '8px'}} /> 
+                        <input type="email" id='email' placeholder='ENTER YOUR EMAIL' style={{width:"250px", height: '35px', textAlign: 'center', fontSize: '10px'}} />
+                        <input type="loccode" id="loccode" placeholder='ENTER LOCATION CODE' style={{width:"250px", height: '35px', textAlign: 'center', fontSize: '10px'}} /> 
                     <button className="grab" style={{width:"200px", height: '35px', marginTop: '20px', margin: '10px', borderRadius: '10px', backgroundColor: 'white'}}>LET'S PLAY</button>
                 </div>
 
-                <div className="form-check" id="checkthreewrap" style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                <div className="form-check" id="checkthreewrap" style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: '20px'}}>
                     <input className="form-check-input" id="checkthree" type="checkbox" value="" style={{backgroundColor: '#061A1B', borderColor: 'white', Size: '8px'}} required/>
                     <label className="form-check-label" style={{fontSize: '8px', color:'white'}}>Agree to terms and conditions.</label>
                 </div>
 
                 <div>
-                    <img id='head' style={{width: '150px', marginTop: '25px', marginBottom: '80px'}} src={GetReady} alt="How Well"/>
+                    <img id='head' style={{width: '150px', marginTop: '45px', marginBottom: '80px'}} src={GetReady} alt="How Well"/>
                 </div>
 
             </div>
