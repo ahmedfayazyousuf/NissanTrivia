97945b319cb44ec4a7f4cfdadce5c0e8fe66553b
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2023-01-05 14:01:16 +0500

    instructions page

--- src/components/Instructions/index.js
+++ src/components/Instructions/index.js
@@ -275,31 +275,17 @@ const Instructions = () => {
 return(
     <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center', textAlign: 'center',backgroundColor: 'white', height: '100vh', width: '100vw', overflowY: 'hidden'}}>
         <div style={{justifyContent: 'center', alignItems: 'center'}}>
-            <img id='head' style={{width: '130px', marginTop: '0px'}} src={NissanLogo} alt="Nissan Logo"/>
+            <img id='head' style={{width: '150px', marginTop: '0px'}} src={NissanLogo} alt="Nissan Logo"/>
         </div>
 
-        <div style={{display: 'flex', flexDirection: 'column', textAlign: 'center', margin: '0', padding: '0', marginTop: '10px'}} >
-            <p className="sss" style={{fontSize: '25px', marginTop: '20px'}} >Instructions</p>
-        </div>
+        <div className="slotparent"  style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center', width: '100%', marginTop: '100px',}}>
 
-        <div className="slotparent"  style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center', width: '100%'}}>
-            
-            <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center', width: '300px',paddingLeft: '0px', paddingTop: '20px', paddingBottom: '20px', border: '2px solid black', borderRadius: '15px'}}>
-
-                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center'}}>
-                    <p>INSTRUCTION INSTRUCTION 1</p>
-                    <p>INSTRUCTION INSTRUCTION 1</p>
-                    <p>INSTRUCTION INSTRUCTION 1</p>
-                    <p>INSTRUCTION INSTRUCTION 1</p>
-                </div>
-            </div>
-            
-
-            <button className="grab" style={{width:"220px", marginTop: '40px', marginBottom: '40px', borderRadius: '10px', padding: '10px', color: 'white', backgroundColor: 'black', cursor: 'grab'}} onClick={handleSubmit}>START</button>
+            <button className="grab" style={{width:"220px", marginTop: '40px', marginBottom: '110px', borderRadius: '10px', padding: '10px', color: 'white', backgroundColor: 'black', cursor: 'grab'}} onClick={handleSubmit}>START</button>
 
             <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
                 <img src="https://firebasestorage.googleapis.com/v0/b/assetuploadformbuilder.appspot.com/o/images%2Fgetreadyexplore.png?alt=media&token=bedd3483-ca06-414d-940e-9185a0b808a1" alt="Logo" style={{width: '20vh', marginTop: '30px', marginBottom: '10px'}}/>
             </div>
+
         </div>
     </div>
 )
